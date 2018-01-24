788df4bf3d7642bbd11913b8bb00b03271caa178
Aria Haghighi <me@aria42.com>
2018-01-24 07:32:22 -0800

    Add tests for mime-convertible

---
+++ test/clojupyter/protocol/mime_convertible_test.clj
@@ -0,0 +1,25 @@
+(ns clojupyter.protocol.mime-convertible-test
+  (:require [clojupyter.protocol.mime-convertible :refer :all]
+            [midje.sweet :refer :all]))
+
+(fact "Should render strings, keywords, and numbers"
+      (to-mime 1) => "{\"text/plain\":\"1\"}"
+      (to-mime "2") => "{\"text/plain\":\"\\\"2\\\"\"}"
+      (to-mime :1) => "{\"text/plain\":\":1\"}"
+      (to-mime ::1) =>
+        "{\"text/plain\":\":clojupyter.protocol.mime-convertible-test/1\"}")
+
+(fact "Should render lazy sequences"
+      (to-mime (map inc [1 2 3])) => "{\"text/plain\":\"(2 3 4)\"}"
+      (to-mime (map keyword ["a" "b" "c"])) => "{\"text/plain\":\"(:a :b :c)\"}")
+
+
+(deftype Custom [])
+
+(defmethod print-method Custom
+  [v ^java.io.Writer w]
+  (.write w "my-custom"))
+
+(fact "Should render deftype toString"
+      (to-mime (Custom.)) => "{\"text/plain\":\"my-custom\"}")
+

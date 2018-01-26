0bc5d5cbb0ae7d173b1e3de1e51bd8be8b1895e8
Aria Haghighi <me@aria42.com>
2018-01-26 01:13:04 -0800

    change incanter display to generic BufferedImage, which covers a broader range of things

--- src/clojupyter/misc/display.clj
+++ src/clojupyter/misc/display.clj
@@ -1,32 +1,11 @@
 (ns clojupyter.misc.display
   (:require [clojupyter.protocol.mime-convertible :as mc]
             [clojupyter.misc.states :as states]
-            [cheshire.core :as cheshire]
-            [clojure.data.codec.base64 :as b64]
-            [clojure.java.io :as io]
-            [hiccup.core :as hiccup])
-  (:import [javax.imageio ImageIO]))
+            [hiccup.core :as hiccup]))
 
 (defn display [obj]
   (swap! (:display-queue @states/current-global-states) conj (mc/to-mime obj))
   nil)
-
-;; Incanter Plot
-
-(defrecord IncanterPlot [chart width height]
-
-  mc/PMimeConvertible
-  (to-mime [plot]
-    (let [out (io/java.io.ByteArrayOutputStream.)
-          {:keys [chart width height]} plot]
-      (ImageIO/write (.createBufferedImage chart width height)
-                     "png" out)
-      (mc/stream-to-string
-       {:image/png (str (apply str (map char (b64/encode (.toByteArray out)))))}))))
-
-(defn make-incanter-plot [chart & {:keys [width height]
-                                   :or {width 600 height 400}}]
-  (IncanterPlot. chart width height))
 
 
 ;; Html
--- src/clojupyter/protocol/mime_convertible.clj
+++ src/clojupyter/protocol/mime_convertible.clj
@@ -1,5 +1,11 @@
 (ns clojupyter.protocol.mime-convertible
-  (:require [cheshire.core :as cheshire]))
+  (:require
+   [cheshire.core :as cheshire]
+   [clojure.data.codec.base64 :as b64]
+   [clojure.java.io :as io])
+  (:import
+   [javax.imageio ImageIO]
+   [java.awt.image.BufferedImage]))
 
 (defn stream-to-string [map]
   (let [repr (java.io.StringWriter.)]
@@ -17,4 +23,11 @@
 
   nil
   (to-mime [o]
-    "nil"))
+    "nil")
+
+  java.awt.image.BufferedImage
+  (to-mime [o]
+    (let [out (io/java.io.ByteArrayOutputStream.)]
+      (ImageIO/write o "png" out)
+      (stream-to-string
+       {:image/png (-> out .toByteArray b64/encode String.)}))))

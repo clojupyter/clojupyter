3e5831dce489fcd4402b8294b83bdce0fb86c18e
Klaus Harbo <klaus@harbo.net>
2019-02-18 06:03:02 +0100

    depend on cider-nrepl 0.5.3 and cider-nrepl 0.20.0

--- project.clj
+++ project.clj
@@ -4,14 +4,14 @@
   :license {:name "MIT"}
   :dependencies [[beckon "0.1.1"]
                  [cheshire "5.7.0"]
-                 [cider/cider-nrepl "0.21.1"]
+                 [cider/cider-nrepl "0.20.0"]
                  [clj-time "0.11.0"]
                  [com.cemerick/pomegranate "1.0.0"]
                  [com.taoensso/timbre "4.8.0"]
                  [net.cgrand/parsley "0.9.3" :exclusions [org.clojure/clojure]]
                  [net.cgrand/sjacket "0.1.1" :exclusions [org.clojure/clojure
                                                           net.cgrand.parsley]]
-                 [nrepl "0.6.0"]
+                 [nrepl "0.5.3"]
                  [org.clojure/clojure "1.10.0"]
                  [org.clojure/core.async "0.2.395"]
                  [org.clojure/data.codec "0.1.0"]
@@ -22,9 +22,9 @@
                  [org.zeromq/jeromq "0.3.4"] ; "0.3.5" (modern) fails on zmq/bind.
                  [pandect "0.5.4"]
                  [hiccup "1.0.5"]]
-  :aot [clojupyter.core]
-  :main clojupyter.core
-  :jvm-opts ["-Xmx250m"]
+  ;:aot [clojupyter.core]
+  ;:main clojupyter.core
+  ;:jvm-opts ["-Xmx250m"]
   :keep-non-project-classes true
   :profiles {:dev {:dependencies [[midje "1.9.0" :exclusions [org.clojure/clojure]]]
                    :plugins [[lein-midje "3.2.1"]]}})

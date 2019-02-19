0a0f8903586f6cb116374af977fa4ad380084730
Klaus Harbo <klaus@harbo.net>
2019-02-19 07:55:18 +0100

    move to deps.edn keeping project.clj

--- project.clj
+++ project.clj
@@ -2,30 +2,14 @@
   :description "An IPython kernel for executing Clojure code"
   :url "http://github.com/roryk/clojupyter"
   :license {:name "MIT"}
-  :dependencies [[beckon "0.1.1"]
-                 [cheshire "5.7.0"]
-                 [cider/cider-nrepl "0.20.0"]
-                 [clj-time "0.11.0"]
-                 [com.cemerick/pomegranate "1.0.0"]
-                 [com.taoensso/timbre "4.8.0"]
-                 [net.cgrand/parsley "0.9.3" :exclusions [org.clojure/clojure]]
-                 [net.cgrand/sjacket "0.1.1" :exclusions [org.clojure/clojure
-                                                          net.cgrand.parsley]]
-                 [nrepl "0.5.3"]
-                 [org.clojure/clojure "1.10.0"]
-                 [org.clojure/core.async "0.2.395"]
-                 [org.clojure/data.codec "0.1.0"]
-                 [org.clojure/data.json "0.2.6"]
-                 [org.clojure/java.jdbc "0.3.7"]
-                 [org.xerial/sqlite-jdbc "3.7.2"]
-                 [org.zeromq/cljzmq "0.1.4" :exclusions [org.zeromq/jzmq]]
-                 [org.zeromq/jeromq "0.3.4"] ; "0.3.5" (modern) fails on zmq/bind.
-                 [pandect "0.5.4"]
-                 [hiccup "1.0.5"]]
-  ;:aot [clojupyter.core]
-  ;:main clojupyter.core
+  :aot [clojupyter.core]
+  :main clojupyter.core
   ;:jvm-opts ["-Xmx250m"]
   :keep-non-project-classes true
   :profiles {:dev {:dependencies [[midje "1.9.0" :exclusions [org.clojure/clojure]]]
-                   :plugins [[lein-midje "3.2.1"]]}})
+                   :plugins [[lein-midje "3.2.1"]]}}
+  :plugins				[[lein-tools-deps	"0.4.1"]]
+  :middleware				[lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+  :lein-tools-deps/config		{:config-files		[:install :user :project]}
+)
 

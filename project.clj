bfc5a4634df669a211694c86de3c4a0979fcd461
Klaus Harbo <klaus@harbo.net>
2019-02-26 14:25:06 +0100

    delete tags/releases - revert to 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojupyter "0.1.4-SNAPSHOT"
+(defproject clojupyter "0.1.1-SNAPSHOT"
   :description "An IPython kernel for executing Clojure code"
   :url "http://github.com/roryk/clojupyter"
   :license {:name "MIT"}

bc66f850a42e40977859a11dcc4342a7e578b654
Klaus Harbo <klaus@harbo.net>
2019-08-23 18:07:05 +0200

    bump version to 0.2.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojupyter "0.2.3"
+(defproject clojupyter "0.2.4-SNAPSHOT"
   :description "A Jupyter kernel for Clojure"
   :url "http://github.com/clojupyter/clojupyter"
   :license {:name "MIT"}

31b122a74a398bea84c46e9cd4865980b946a3a9
Klaus Harbo <klaus@harbo.net>
2020-04-04 21:11:46 +0200

    Add src/clojupyter/widgets/{alpga,ipywidgets}.clj

---
+++ src/clojupyter/widgets/alpha.clj
@@ -0,0 +1,52 @@
+(ns clojupyter.widgets.alpha
+  (:require
+   [camel-snake-kebab.core :as csk]
+   [clojupyter.kernel.comm-atom :as ca]
+   [clojupyter.kernel.jup-channels :as jup]
+   [clojupyter.log :as log]
+   [clojupyter.messages :as msgs]
+   [clojupyter.state :as state]
+   [clojupyter.util :as u]
+   [clojupyter.util-actions :as u! :refer [uuid]]
+   [clojupyter.widgets.ipywidgets :as ipy]
+   [clojure.edn :as edn]
+   [clojure.spec.alpha :as s]
+   [clojure.spec.test.alpha :refer [instrument unstrument]]
+   [clojure.string :as str]
+   [io.simplect.compose :refer [def- c C p P >->> >>->]]
+   ))
+
+;;; ------------------------------------------------------------------------------------------------------------------------
+;;; DISPLAY
+;;; ------------------------------------------------------------------------------------------------------------------------
+
+(defn display
+  [comm-atom]
+  (log/debug "display -- " (log/ppstr {:comm-atom comm-atom}))
+  (if (ca/comm-atom? comm-atom)
+    (let [{:keys [jup req-message]} (state/current-context)]
+      (assert (and jup req-message))
+      (jup/send!! jup :iopub_port req-message msgs/DISPLAY-DATA
+                  (msgs/display-data-content (ca/comm-id comm-atom))))
+    (throw (Exception. (str "Not a comm-atom: " comm-atom)))))
+
+;;; ------------------------------------------------------------------------------------------------------------------------
+;;; INTERACT
+;;; ------------------------------------------------------------------------------------------------------------------------
+
+(defn interactive
+  [output-widget f wmap]
+  (let [agent-key (gensym)
+        cur (atom (into {} (for [[k w] wmap] [k (get @w :value)])))
+        set-value! (fn [] (swap! output-widget assoc :value (f @cur)))]
+    (doseq [[k w] wmap]
+      (assert (ca/comm-atom? w))
+      (ca/watch w agent-key
+                (fn [_ _ _ new]
+                  (try (swap! cur assoc k (get new :value))
+                       (set-value!)
+                       (catch Exception e
+                         (log/error (str "Error in interactive-agent@" agent-key ": " e)
+                                    (log/ppstr {:cur @cur :k k :new new})))))))
+    (set-value!)
+    (ipy/v-box {:children (vec (concat (vals wmap) [output-widget]))})))

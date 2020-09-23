d907f733b80389c1422a034e05b921fb8843665e
Daniel Ciumberică <ciumbi.ionut@gmail.com>
2020-09-22 21:27:25 -0400

    Moved dependencies to project.clj

--- project.clj
+++ project.clj
@@ -2,10 +2,39 @@
   :description "A Jupyter kernel for Clojure"
   :license     {:name "MIT"}
   :url         "https://github.com/clojupyter/clojupyter"
-
   :scm         {:name "git" :url "https://github.com/clojupyter/clojupyter"}
-  :source-paths      [] ;; provided by lein-tools-deps
-  :resource-paths    [] ;; provided by lein-tools-deps
+  :source-paths      ["src"]
+  :resource-paths    ["resources"]
+
+  :dependencies      [[camel-snake-kebab "0.4.1"]
+                      [cheshire "5.10.0"]
+                      [cider/cider-nrepl "0.25.3"]
+                      [clojure.java-time "0.3.2"]
+                      [com.cemerick/pomegranate "1.1.0"]
+                      [com.grammarly/omniconf "0.4.2"]
+                      [com.taoensso/timbre "4.10.0"]
+                      [hiccup "1.0.5"]
+                      [io.aviso/pretty "0.1.37"]
+                      [io.pedestal/pedestal.interceptor "0.5.8"]
+                      [io.simplect/compose "0.7.27"]
+                      [net.cgrand/parsley "0.9.3" :exclusions [org.clojure/clojure]]
+                      [net.cgrand/regex "1.1.0" :exclusions [org.clojure/clojure]]
+                      [org.clojars.trptcolin/sjacket "0.1.1.1" :exclusions [org.clojure/clojure net.cgrand.parsley]]
+                      [nrepl "0.8.1"]
+                      [org.clojure/clojure "1.10.1"]
+                      [org.clojure/data.codec "0.1.1"]
+                      [org.clojure/java.jdbc "0.7.11"]
+                      [org.xerial/sqlite-jdbc "3.32.3.2"]
+                      [org.zeromq/jeromq "0.5.2"]
+                      [pandect "0.6.1"]
+                      [slingshot "0.12.2"]
+                      [zprint "1.0.0"]
+                      [org.clojure/core.async "1.3.610"]
+                      [com.fzakaria/slf4j-timbre "0.3.19"]
+                      [org.slf4j/log4j-over-slf4j "1.7.30"]
+                      [org.slf4j/jul-to-slf4j "1.7.30"]
+                      [org.slf4j/jcl-over-slf4j "1.7.30"]]
+
   :profiles    {:dev     {:dependencies [[midje "1.9.9" :exclusions [org.clojure/clojure]]
                                          [me.raynes/fs "1.4.6"]
                                          [io.forward/yaml "1.0.10" :exclusions [org.flatland/ordered]]
@@ -19,12 +48,4 @@
                 :uberjar {:aot :all}
                 :metajar {:jvm-opts ["-Dclojure.compiler.direct-linking=true"]}}
 
-  :main        clojupyter.kernel.core
-
-  ;; Use tools-deps for dependencies:
-  :plugins     [[lein-tools-deps "0.4.5"]]
-  :middleware  [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
-  :lein-tools-deps/config  {:config-files [:install :user :project]}
-
-  ;; The aliases below can be invoked with 'lein <alias>':
-  :aliases      {"clojupyter"           ["run" "-m" "clojupyter.cmdline"]})
+  :main        clojupyter.kernel.core)

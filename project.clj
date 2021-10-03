0505687af60ce5993a7a6c2ebf7d3b909a95845d
Karl Krukow <krukow@github.com>
2021-10-03 14:13:31 +0200

    Fix dynamic version load

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojupyter "0.3.3-alpha"
+(defproject clojupyter "0.3.3-alpha2"
   :description			"A Jupyter kernel for Clojure"
   :license			{:name "MIT"}
   :url				"https://github.com/clojupyter/clojupyter"

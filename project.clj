923a81baab069e0cda5f27500525b31f47197851
Daniel Ciumberică <ciumbi.ionut@gmail.com>
2021-11-18 12:15:04 -0500

    Bumped deps versions.

--- project.clj
+++ project.clj
@@ -7,40 +7,40 @@
   :resource-paths    ["resources"]
 
   :dependencies      [;; Core
-                      [org.clojure/clojure "1.10.1"]
+                      [org.clojure/clojure "1.10.3"]
                       ;; Networking
                       [nrepl "0.8.3"]
                       [org.zeromq/jeromq "0.5.2"]
-                      [org.clojure/core.async "1.3.618"]
-                      [cider/cider-nrepl "0.26.0"]
+                      [org.clojure/core.async "1.4.627"]
+                      [cider/cider-nrepl "0.27.2"]
                       ;; Extra
-                      [camel-snake-kebab "0.4.1"]
-                      [cheshire "5.10.0"]
-                      [com.grammarly/omniconf "0.4.2"]
-                      [clojure.java-time "0.3.2"]
+                      [camel-snake-kebab "0.4.2"]
+                      [cheshire "5.10.1"]
+                      [com.grammarly/omniconf "0.4.3"]
+                      [clojure.java-time "0.3.3"]
                       [hiccup "1.0.5"]
-                      [io.aviso/pretty "0.1.37"]        ;; Exceptions pretty printing
-                      [pandect "0.6.1"]                 ;; Message signing
+                      [io.aviso/pretty "1.1"]        ;; Exceptions pretty printing
+                      [pandect "1.0.2"]                 ;; Message signing
                       ;; Dependencies Management
                       [com.cemerick/pomegranate "1.1.0"]
                       ;; Logging
                       [com.taoensso/timbre "5.1.2"]
-                      [com.fzakaria/slf4j-timbre "0.3.19"]
-                      [org.slf4j/log4j-over-slf4j "1.7.30"]
-                      [org.slf4j/jul-to-slf4j "1.7.30"]
-                      [org.slf4j/jcl-over-slf4j "1.7.30"]
+                      [com.fzakaria/slf4j-timbre "0.3.21"]
+                      [org.slf4j/log4j-over-slf4j "1.7.32"]
+                      [org.slf4j/jul-to-slf4j "1.7.32"]
+                      [org.slf4j/jcl-over-slf4j "1.7.32"]
                       ;; History
-                      [org.clojure/java.jdbc "0.7.11"]
-                      [org.xerial/sqlite-jdbc "3.32.3.2"]
+                      [org.clojure/java.jdbc "0.7.12"]
+                      [org.xerial/sqlite-jdbc "3.36.0.3"]
                       ;; Framework
-                      [io.pedestal/pedestal.interceptor "0.5.8"]
+                      [io.pedestal/pedestal.interceptor "0.5.9"]
                       [io.simplect/compose "0.7.27"]
                       [net.cgrand/parsley "0.9.3" :exclusions [org.clojure/clojure]]
                       [org.clojars.trptcolin/sjacket "0.1.1.1" :exclusions [org.clojure/clojure net.cgrand.parsley]]
                       ;; Dev
                       [net.cgrand/regex "1.1.0" :exclusions [org.clojure/clojure net.cgrand.parsley]]
                       [slingshot "0.12.2"]
-                      [zprint "0.4.15"]]
+                      [zprint "1.1.2"]]
 
   :profiles    {:dev     {:dependencies [[midje "1.9.9" :exclusions [org.clojure/clojure]]
                                          [org.clojure/test.check "1.1.0"]

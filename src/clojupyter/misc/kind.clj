0038b8b9cea80fab59f4e37c52d3a458d0a5c61e
Carsten Behring <carsten.behring@gmail.com>
2025-01-06 17:15:46 +0100

    removed comment

--- src/clojupyter/misc/kind.clj
+++ src/clojupyter/misc/kind.clj
@@ -152,31 +152,3 @@
     ;(println :advising--meta-value (meta value ))
     ))
 
-(comment
-
-  (def ct
-   (kind/cytoscape
-    {:elements {:nodes [{:data {:id "a" :parent "b"} :position {:x 215 :y 85}}
-                        {:data {:id "b"}}
-                        {:data {:id "c" :parent "b"} :position {:x 300 :y 85}}
-                        {:data {:id "d"} :position {:x 215 :y 175}}
-                        {:data {:id "e"}}
-                        {:data {:id "f" :parent "e"} :position {:x 300 :y 175}}]
-                :edges [{:data {:id "ad" :source "a" :target "d"}}
-                        {:data {:id "eb" :source "e" :target "b"}}]}
-     :style [{:selector "node"
-              :css {:content "data(id)"
-                    :text-valign "center"
-                    :text-halign "center"}}
-             {:selector "parent"
-              :css {:text-valign "top"
-                    :text-halign "center"}}
-             {:selector "edge"
-              :css {:curve-style "bezier"
-                    :target-arrow-shape "triangle"}}]
-     :layout {:name "preset"
-              :padding 5}}) 
-  )
-
-  (to-hiccup-js/render {:value ct})
-  )

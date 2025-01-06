875b7688d99ef94bec7ab2719ecde1a728816969
Carsten Behring <carsten.behring@gmail.com>
2025-01-06 16:48:56 +0100

    fixed syntax

--- src/clojupyter/misc/kind.clj
+++ src/clojupyter/misc/kind.clj
@@ -108,6 +108,7 @@
            display/hiccup-html)
 
 
+          (display-default value)
 
           )
            
@@ -116,16 +117,6 @@
     hiccup
     ))
 
-(to-hiccup/render {:value (kind/code "(+ 1 1)")})
-;;=> {:value ["(+ 1 1)"],
-;;    :meta-kind :kind/code,
-;;    :kindly/options {},
-;;    :kind :kind/code,
-;;    :advice [[:kind/code {:reason :metadata}] [:kind/vector {:reason :predicate}] [:kind/seq {:reason :predicate}]],
-;;    :deps #{:kind/code},
-;;    :hiccup [:pre {:class "kind-code"} [:code {:class "sourceCode"} nil]]}
-
-(display-default (kind/code {:a 1}))
 
 (defn kind-eval [form]
   
@@ -138,46 +129,47 @@
     ;(println :advising--meta-value (meta value ))
     ))
 
+(comment
+
+  (def hs
+    (kind/highcharts
+     {:title {:text "Line chart"}
+      :subtitle {:text "By Job Category"}
+      :yAxis {:title {:text "Number of Employees"}}
+      :series [{:name "Installation & Developers"
+                :data [43934, 48656, 65165, 81827, 112143, 142383,
+                       171533, 165174, 155157, 161454, 154610]}
+
+               {:name "Manufacturing",
+                :data [24916, 37941, 29742, 29851, 32490, 30282,
+                       38121, 36885, 33726, 34243, 31050]}
+
+               {:name "Sales & Distribution",
+                :data [11744, 30000, 16005, 19771, 20185, 24377,
+                       32147, 30912, 29243, 29213, 25663]}
+
+               {:name "Operations & Maintenance",
+                :data [nil, nil, nil, nil, nil, nil, nil,
+                       nil, 11164, 11218, 10077]}
+
+               {:name "Other",
+                :data [21908, 5548, 8105, 11248, 8989, 11816, 18274,
+                       17300, 13053, 11906, 10073]}]
+
+      :xAxis {:accessibility {:rangeDescription "Range: 2010 to 2020"}}
+
+      :legend {:layout "vertical",
+               :align "right",
+               :verticalAlign "middle"}
+
+      :plotOptions {:series {:label {:connectorAllowed false},
+                             :pointStart 2010}}
+
+      :responsive {:rules [{:condition {:maxWidth 500},
+                            :chartOptions {:legend {:layout "horizontal",
+                                                    :align "center",
+                                                    :verticalAlign "bottom"}}}]}}))
 
 
-(def hs
-  (kind/highcharts
-   {:title {:text "Line chart"}
-    :subtitle {:text "By Job Category"}
-    :yAxis {:title {:text "Number of Employees"}}
-    :series [{:name "Installation & Developers"
-              :data [43934, 48656, 65165, 81827, 112143, 142383,
-                     171533, 165174, 155157, 161454, 154610]}
-
-             {:name "Manufacturing",
-              :data [24916, 37941, 29742, 29851, 32490, 30282,
-                     38121, 36885, 33726, 34243, 31050]}
-
-             {:name "Sales & Distribution",
-              :data [11744, 30000, 16005, 19771, 20185, 24377,
-                     32147, 30912, 29243, 29213, 25663]}
-
-             {:name "Operations & Maintenance",
-              :data [nil, nil, nil, nil, nil, nil, nil,
-                     nil, 11164, 11218, 10077]}
-
-             {:name "Other",
-              :data [21908, 5548, 8105, 11248, 8989, 11816, 18274,
-                     17300, 13053, 11906, 10073]}]
-
-    :xAxis {:accessibility {:rangeDescription "Range: 2010 to 2020"}}
-
-    :legend {:layout "vertical",
-             :align "right",
-             :verticalAlign "middle"}
-
-    :plotOptions {:series {:label {:connectorAllowed false},
-                           :pointStart 2010}}
-
-    :responsive {:rules [{:condition {:maxWidth 500},
-                          :chartOptions {:legend {:layout "horizontal",
-                                                  :align "center",
-                                                  :verticalAlign "bottom"}}}]}}))
-
-
-(to-hiccup-js/render {:value hs})
+  (to-hiccup-js/render {:value hs})
+  )
